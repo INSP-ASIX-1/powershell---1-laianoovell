6f55bc8f6bfad804e44168c74f019b3be54f1f3f
laianoovell <93976923+laianoovell@users.noreply.github.com>
2022-02-11 18:37:37 +0100

    laia

--- 03.ps1
+++ 03.ps1
@@ -1,1 +1,2 @@
 #Crea la carpeta myTest des de PowerShell, al mateix lloc on et trobes
+Test-Path myTest
--- 04.ps1
+++ 04.ps1
@@ -1,1 +1,2 @@
 #Crea el fitxer test.txt des de PowerShell
+New-Item -Path 'test.txt' -ItemType File
